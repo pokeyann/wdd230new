edfe1b4bb4e9cfc51535d602f5ff7e9c5e4b5825
pokeyann <pokeyann@gmail.com>
2022-10-27 21:56:05 -0500

    image sizes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,20 +1,13 @@
-// f = wind chill factor in fahrenheit
-// t = air average temperature
-// s = wind speed in miles per hour
-// formula = f = 35.74 + 0.6215t - 35.75s raised to 0.16 + 0.4275 t s raised to 0.16
-//Wind Chill Specification Limits: Wind Chill Temperature is officially defined for temperatures at or below
-// 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph). <=50°F and >3.0mph
-
-const temp = 50;
+const temp = 71;
 const windspeed = 8;
 const windchill = (35.74 + (0.6215*temp)) - (35.75*(windspeed**0.16)) + (0.4275*temp*(windspeed**0.16));
 
-  document.querySelector("#windchill").textContent = Math.round(windchill);
+document.getElementById("temp").innerHTML = temp;
 
 if (temp <= 50 && windspeed >= 3) { 
-  console.log(windchill);
+  document.getElementById("windchill").innerHTML = Math.round(windchill);
 }
 else {
-  console.log("N/A");
+  document.getElementById("windchill").innerHTML = "N/A"; 
 }
 
