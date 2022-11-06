835bcabe31d60851279af734b610398499160226
pokeyann <pokeyann@gmail.com>
2022-11-05 20:03:30 -0500

    discover page

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -23,6 +23,12 @@ function toggleMenu() {
   document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+//discover page timestamp in footer
+const todayDisplay = document.querySelector(".today");
+
+todayDisplay.textContent = Date.now();
+
+
 
 
 
